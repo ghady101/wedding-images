a46ed43871176941baca154b6709c11e52a584a7
ghady <ghadygergie0@gmail.com>
2024-06-22 14:49:35 +0300

    css fix

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useRef } from 'react';
 import { useKey } from '../context/KeyContext';
 
 function Protected({ children }) {
 	const { key, setKey } = useKey();
+	const inputRef = useRef(null);
+
+	useEffect(() => {
+		if (inputRef.current) {
+			inputRef.current.focus();
+		}
+	}, []);
 
 	if (key === '123') return children;
 
 	return (
 		<form>
 			<input
+				ref={inputRef}
 				className='protected-input'
 				type='password'
 				value={key}
